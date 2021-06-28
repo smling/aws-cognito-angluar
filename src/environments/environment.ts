b318358c77945bb5a373ea33647e76cc53080107
smling <smling9_9@hotmail.com>
2021-06-28 21:50:46 +0800

    update settings.

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,11 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  poolConfig : {
+    UserPoolId: 'Your user pool', // Your user pool id here
+    AppClientId: 'Your Client Id' // Your client id here  
+  }
 };
 
 /*
